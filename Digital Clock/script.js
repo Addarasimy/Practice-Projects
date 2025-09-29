3ddf8270cf397e3f4fa921ae01bd329661b4e8db
user.name: "addarasimy25@gmail.com <addarasimy25@gmail.com>
2025-09-29 12:54:52 +0100

    final update

---
+++ Digital Clock/script.js
@@ -0,0 +1,33 @@
+const hourEl = document.querySelector('.hour');
+
+const minuteEl = document.querySelector('.minute');
+
+const secondEl = document.querySelector('.second');
+
+const ampmEl = document.querySelector('.clock-time');
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h > 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+        updateClock();
+    },1000)
+
+}
+
+updateClock();
